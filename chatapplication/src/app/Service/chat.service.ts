805a10ff7537b9bb0d2b62f06c7dda868ffd06db
Priyanka Patel <38903077+patelpriyanka123@users.noreply.github.com>
2021-12-31 06:18:40 +0530

    update

--- chatapplication/src/app/Service/chat.service.ts
+++ chatapplication/src/app/Service/chat.service.ts
@@ -2,19 +2,33 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { io } from 'socket.io-client';
 import { SendReceiveMsg } from '../model/sendReceiveMsg';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
   public message$: BehaviorSubject<SendReceiveMsg> = new BehaviorSubject(new SendReceiveMsg());
+  public user$: BehaviorSubject<User> = new BehaviorSubject(new User());
   constructor() {}
 
   socket = io('http://localhost:3000');
 
+  public sendUser(user: User) {
+    this.socket.emit('user', user);
+  }
+
   public sendMessage(message: SendReceiveMsg) {
     this.socket.emit('message', message);
   }
+
+  public getUser = () => {
+    this.socket.on('user', (user) =>{
+      this.message$.next(user);
+    });
+    
+    return this.user$.asObservable();
+  };
 
   public getNewMessage = () => {
     this.socket.on('message', (message) =>{
--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const app = require('express')();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
-  cors: {origin : '*'}
+  cors: { origin: '*' }
 });
 
 const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('user', (user) => {
+    console.log(user);
+    io.emit('user', user);
+  });
 
   socket.on('message', (message) => {
     console.log(message);
